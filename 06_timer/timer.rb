258c38974e0a01c082c224187245bc8db67e872f
Daniel Prince <danielahprince@live.com>
2019-05-29 21:15:21 +0200

    soved 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,20 @@
 class Timer
   #write your code here
+  attr_writer :seconds
+  attr_reader :time_string, :seconds
+  def initialize()
+    @seconds = 0
+  end
+
+  def seconds=(s)
+    @seconds = s
+  end
+
+  def time_string
+    hours = @seconds/(60 * 60)
+    minutes = (@seconds - hours * (60 * 60)) / 60
+    seconds = (@seconds - hours * (60 * 60) - minutes * 60)
+    result = "%02d:%02d:%02d" % [hours, minutes, seconds]
+  end
+
 end
