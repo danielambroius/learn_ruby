54907274c9f8cfb9e3b5904709b7cae61e4d1b4e
Daniel Prince <danielahprince@live.com>
2019-05-29 20:17:22 +0200

    Solved 04_pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,22 @@
 #write your code here
+
+def translate(phrase)
+    vowels = "aeiou".split("")
+    consonants = ("a".."z").to_a - vowels
+    arr = phrase.split(" ").map do |word|
+        compareWord = word.downcase  # So that we can comprate to and modify without worrying about transforming to and from uppercase
+        if (vowels.include? compareWord[0])
+            word = word + "ay"
+        else 
+            i = 1
+            while (consonants.include?(compareWord[i]))
+                i += 1
+            end
+            if (compareWord[i-1..i] == "qu") # Special case
+                i += 1
+            end
+            word = word[i..] + word[0..i-1] + "ay"
+        end
+    end
+    arr.join(" ")
+end
