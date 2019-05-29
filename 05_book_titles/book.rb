4a234d2d9d8b3fdb6784fe5934bebcbf3c7bc621
Daniel Prince <danielahprince@live.com>
2019-05-29 20:29:18 +0200

    solved 05_book

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,22 @@
 class Book
 # write your code here
+    attr_reader :title
+    @@nonCapitalized = ["over", "and", "the", "in", "of", "a", "an"]
+
+    def titleize(string)
+        arr = string.split(" ")
+        arr = [arr[0].capitalize] + arr[1..].map do |s|
+            if (@@nonCapitalized.include? s)
+                s
+            else
+                s.capitalize
+            end
+        end
+        return arr.join(" ")
+    end
+
+    def title=(t)
+        @title = titleize(t)
+    end
+
 end
