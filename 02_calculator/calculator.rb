efb2e4a618ed99afd9146c51b536e203cb0cd0ef
Daniel Prince <danielahprince@live.com>
2019-05-29 18:03:37 +0200

    solved 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,30 @@
 #write your code here
+def add(l, r)
+    result = l + r
+end
+
+def subtract(l, r)
+    return l - r 
+end
+
+def sum(array)
+    return array.reduce(0) {|x, y| x + y}
+end
+
+def multiply(*numbers)
+    puts numbers.class
+    return numbers.reduce(1) {|prod, x| prod * x}
+end
+
+def power(a, b)
+    return a ** b
+end
+
+def factorial(n)
+    if (n<0)
+        return nil
+    else
+        return (1..n).reduce(1) {|prod, x| prod * x}
+    end
+end
+
